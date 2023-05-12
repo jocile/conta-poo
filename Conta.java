38f9387a68e80eb5beb6e6b118e89aa2f5a4388d
Jocilé Serra <jocile@gmail.com>
2023-05-12 20:33:44 -0300

    Classe inicial

---
+++ Conta.java
@@ -0,0 +1,27 @@
+public class Conta {
+    private int numero;
+    private String nomeAgencia;
+    private int numeroAgencia;
+    private String nomeBanco;
+    private int numeroBanco;
+    private String tipoConta;
+    private float saldo;
+    
+    public void abrir() {
+    }
+
+    public void encerrar() {
+    }
+
+    public void consultar() {
+    }
+
+    public void saldo() {
+    }
+
+    public void creditar() {
+    }
+
+    public void debitar() {
+    }
+}
