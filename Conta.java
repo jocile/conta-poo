bd79246365a654119a3a0d22f6c7ff9bd7c80870
Jocilé Serra <jocile@gmail.com>
2023-05-12 21:07:49 -0300

    Adicionada a função abrir contar

--- Conta.java
+++ Conta.java
@@ -7,7 +7,24 @@ public class Conta {
     private String tipoConta;
     private float saldo;
     
-    public void abrir() {
+    
+    public static void main(String[] args) {
+        Conta conta1 = new Conta();
+        conta1.abrir(1, "AGSobral", 10, "BB",
+                10, "Corrente");
+        System.out.println(conta1.toString());
+    }
+    //ABRIR → método que recebe todos os atributos e insere 0,0 no saldo;
+    public void abrir(int numero, String nomeAgencia, int numeroAgencia,
+            String nomeBanco, int numeroBanco,String tipoConta) {
+        
+        this.numero = numero;
+        this.nomeAgencia = nomeAgencia;
+        this.numeroAgencia = numeroAgencia;
+        this.nomeBanco = nomeBanco;
+        this.numeroBanco = numeroBanco;
+        this.tipoConta = tipoConta;
+        this.saldo = 0;
     }
 
     public void encerrar() {
@@ -24,4 +41,11 @@ public class Conta {
 
     public void debitar() {
     }
+
+    @Override
+    public String toString() {
+        return ("Conta criada com numero:" + numero + ", na agencia: " + nomeAgencia);
+    }
+    
+    
 }
